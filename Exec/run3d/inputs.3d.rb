730452d327a76257f42bc5f1e539e133c16afeeb
Mu-Hua Chien <mhc431@log-2.nyu.cluster>
2021-11-19 14:16:05 -0500

    fixes input file

--- Exec/run3d/inputs.3d.rb
+++ Exec/run3d/inputs.3d.rb
@@ -72,19 +72,19 @@ prob.perturbation_amplitude = 20
 amr.blocking_factor     = 8
 amr.ref_ratio           = 2 2 2 2
 
-mac.v     = 0
-mac.semicoarsening = 1
-mac.max_semicoarsening_level = 2
+mac_proj.verbose     = 0
+mac_proj.semicoarsening = 1
+mac_proj.max_semicoarsening_level = 2
 
 diffuse.v = 0
 diffuse.semicoarsening = 1
 diffuse.max_semicoarsening_level = 2
 
-proj.v = 0
-proj.max_coarsening_level = 5
-proj.semicoarsening = 1
-proj.max_semicoarsening_level = 1
-proj.proj_tol = 1.e-12
+nodal_proj.verbose = 0
+nodal_proj.max_coarsening_level = 5
+nodal_proj.semicoarsening = 1
+nodal_proj.max_semicoarsening_level = 1
+nodal_proj.proj_tol = 1.e-12
 #nodal_proj.verbose = 5
 #nodal_proj.bottom_verbose = 2
 nodal_proj.bottom_solver = hypre
