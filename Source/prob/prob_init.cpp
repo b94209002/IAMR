62d72eb8370ce526ea4c87b3244592571e520b20
Mu-Hua Chien <mhc431@log-3.hpc.nyu.edu>
2025-04-08 20:58:18 -0400

    fix HIP SYCL complier ii

--- Source/prob/prob_init.cpp
+++ Source/prob/prob_init.cpp
@@ -780,13 +780,13 @@ void NavierStokes::init_RayleighBenard (Box const& vbx,
   amrex::ParallelFor(vbx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
   {
     Real z = problo[2] + (k - domlo.z + 0.5)*dx[2];
-
+    RandomEngine rand_eng{&engine};
     scal(i,j,k,0) = 1.0;
-    Real pert = IC.pertamp * amrex::Random(engine);
+    Real pert = IC.pertamp * amrex::Random(rand_engine);
     scal(i,j,k,1) = pert*exp(-z/dx[2]);
     if (nscal >= 2 )
     {
-      pert = IC.pertamp * amrex::Random(engine);
+      pert = IC.pertamp * amrex::Random(rand_engine);
       scal(i,j,k,2) = pert*exp(-z/dx[2]);
     }
   });
